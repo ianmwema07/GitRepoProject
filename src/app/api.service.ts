b8ef3dbfe8f7d442bd47181fe707aff365f2010e
iano <ianmwema07@gmail.com>
2022-04-16 23:12:00 +0300

    add access token

--- src/app/api.service.ts
+++ src/app/api.service.ts
@@ -1,11 +1,19 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { User } from './user';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
+  user: User;
 
-  constructor() { }
+  constructor(private http:HttpClient) {
+    this.user = new User();
+   }
+
+  getRepo( username: string  ):any{
+    return this.http.get(`https://api.github.com/users/${username}/repos`)
+  }
 }
--- src/app/users/users.component.ts
+++ src/app/users/users.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+
 
 @Component({
   selector: 'app-users',
   templateUrl: './users.component.html',
-  styleUrls: ['./users.component.css']
+  styleUrls: ['./users.component.css'],
+  providers: [ApiService]
 })
 export class UsersComponent implements OnInit {
 
