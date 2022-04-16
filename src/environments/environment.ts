6bbf485feb589741dc876c227da6e934dd8bdcc8
iano <ianmwema07@gmail.com>
2022-04-16 20:41:15 +0300

    add changes to the environment

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  // 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
 };
 
 /*
