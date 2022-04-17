aa0b088efe27cbd587a2b68de2d7b0a671dc1296
iano <ianmwema07@gmail.com>
2022-04-18 00:42:02 +0300

    add pipe

--- src/app/day-count.pipe.ts
+++ src/app/day-count.pipe.ts
@@ -5,8 +5,17 @@ import { Pipe, PipeTransform } from '@angular/core';
 })
 export class DayCountPipe implements PipeTransform {
 
-  transform(value: unknown, ...args: unknown[]): unknown {
-    return null;
+  transform(value: string): number{
+    let created_at = Date.parse(value)
+    let today = new Date();
+    let todayWithNoTime: any = new Date(today.getFullYear(), today.getMonth(), today.getDate());
+    var dateDifference = Math.abs(created_at - todayWithNoTime) //returns value in miliseconds
+    const secondsInDay = 86400; //60 seconds * 60 minutes in an hour * 24 hours in a day
+    var dateDifferenceSeconds = dateDifference*0.001; //converts miliseconds to seconds
+    var dateCounter = dateDifferenceSeconds/secondsInDay;
+
+
+  return Math.ceil(dateCounter);
   }
 
 }
