387dfb22455b3fbe6b417b8958cc77b32c323096
iano <ianmwema07@gmail.com>
2022-04-19 00:38:14 +0300

    add changes to the navigation bar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,12 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { UsersComponent } from './users/users.component';
 import { ReposComponent } from './repos/repos.component';
+import { HomeComponent } from './home/home.component';
 
 
 
 const routes: Routes = [
+  { path:'', component:HomeComponent },
   { path:'users', component:UsersComponent },
-  { path:'repos', component:ReposComponent }
+  { path:'repos', component:ReposComponent },
 ];
 
 @NgModule({
